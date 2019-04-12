b9ba49af5f099f19ada038d3cf50b0263912d43a
Allan Jone <joeallan123@gmail.com>
2019-04-12 23:20:38 +0530

    + Tested JikanApi.Net + Added demo anime search page + Added demo anime search methods + Added demo anime details page + Added demo anime details methods

---
+++ AnimeSearchApp/AnimeSearchApp/Views/AnimeDetailsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimeSearchApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AnimeDetailsPage : ContentPage
+	{
+        IJikan jikan = new Jikan(true);
+        private AnimeSearchEntry _anime;
+
+        public AnimeDetailsPage(AnimeSearchEntry anime)
+        {
+            if (anime == null)
+                throw new ArgumentNullException(nameof(anime));
+
+            _anime = anime;
+
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            BindingContext = await jikan.GetAnime(_anime.MalId);
+
+            base.OnAppearing();
+        }
+    }
+}
---
+++ AnimeSearchApp/AnimeSearchApp/Views/AnimesPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using JikanDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnimeSearchApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AnimesPage : ContentPage
+    {
+        private IJikan jikan = new Jikan(true);
+
+        private static readonly int MinLength = 3;
+
+        private BindableProperty IsSearchingProperty =
+            BindableProperty.Create("IsSearching", typeof(bool), typeof(AnimesPage), false);
+
+        public bool IsSearching
+        {
+            get { return (bool)GetValue(IsSearchingProperty); }
+            set { SetValue(IsSearchingProperty, value); }
+        }
+        public AnimesPage()
+        {
+            BindingContext = this;
+
+            InitializeComponent();
+        }
+
+        async void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == null || e.NewTextValue.Length < MinLength)
+                return;
+
+            await FindAnimes(animeString: e.NewTextValue);
+        }
+
+        async Task FindAnimes(string animeString)
+        {
+            try
+            {
+                IsSearching = true;
+                var animeSearchEntry = await jikan.SearchAnime(animeString);
+                var animes = new List<AnimeSearchEntry>(animeSearchEntry.Results);
+                animesListView.ItemsSource = animes;
+                animesListView.IsVisible = animes.Any();
+                notFound.IsVisible = !animesListView.IsVisible;
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Error", "Could not retrive the lsit of animes", "OK");
+            }
+            finally
+            {
+                IsSearching = false;
+            }
+        }
+
+        async void OnAnimeSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem == null)
+                return;
+
+            var anime = e.SelectedItem as AnimeSearchEntry;
+
+            animesListView.SelectedItem = null;
+
+            await Navigation.PushAsync(new AnimeDetailsPage(anime));
+
+        }
+    }
+}
